49e528d1df215c160ef033e5abaa9a1711300758
Edgar Huizar <edgarhuizar313@gmail.com>
2025-04-01 18:58:46 -0600

    style: unify color variables and improve button styling

--- frontend/src/Components/SignInBanner/SignInBanner.jsx
+++ frontend/src/Components/SignInBanner/SignInBanner.jsx
@@ -1,21 +1,21 @@
-import './SignInBanner.css';
+import Styles from './SignInBanner.module.css';
 
 const SignInBanner = () => {
     return (
-        <article className="signInBannerContainer">
-            <div className="signInBanner">
+        <article className={Styles.signInBannerContainer}>
+            <div className={Styles.signInBanner}>
                 <img 
                     src="/Graphics/SignInImage.png" 
                     alt="" 
                     style={{width: '100%', marginBottom: '10vh'}}
                     draggable="false" 
                 />
-                <div className="signInBannerBlur"></div>
-                <section className='signInBannerInfo'>
-                    <h1 className='signInTitle'>Tu proximo roomie a un click de distancia</h1>
-                    <p className="signInDescription">Las mejores habitaciones y roomies te están esperando. Inicia sesión para encontrarlos.</p>
-                    <button className="signInButton">Iniciar Sesión</button>
-                    <button className="signUpButton">Registrarse</button>
+                <div className={Styles.signInBannerBlur}></div>
+                <section className={Styles.signInBannerInfo}>
+                    <h1 className={Styles.signInTitle}>Tu proximo roomie a un click de distancia</h1>
+                    <p className={Styles.signInDescription}>Las mejores habitaciones y roomies te están esperando. Inicia sesión para encontrarlos.</p>
+                    <button className={Styles.signInButton}>Iniciar Sesión</button>
+                    <button className={Styles.signUpButton}>Registrarse</button>
                 </section>
 
             </div>

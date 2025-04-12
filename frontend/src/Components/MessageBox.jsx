f00866f1a7a5734732d7cd6948a7b6009ea70b49
Edgar Huizar <edgarhuizar313@gmail.com>
2025-04-12 15:10:50 -0600

    style(MessageBox): fix text overflow by enabling word wrapping and line breaks

--- frontend/src/Components/MessageBox.jsx
+++ frontend/src/Components/MessageBox.jsx
@@ -1,15 +1,24 @@
 import '../Styles/MessageBox.css';
 
-const MessageBox = ({messages, info, chatUserId, user}) =>{
-
-    const flag = true;
+const MessageBox = ({ messages, chatUserId, user }) => {
     return (
-        <div className="messageBoxContainer" style={messages.msg_for === user ? {justifyContent: 'flex-end'} : {justifyContent: 'flex-start'}}>
-            <div className={`messageBox ${messages.msg_for === user ? 'sentMessageBox' : 'receivedMessageBox'}`}>
-                <p className='messageText'>{messages.msg_content}</p>
+        <div
+            className="messageBoxContainer"
+            style={
+                messages.msg_for === user
+                    ? { justifyContent: 'flex-end' }
+                    : { justifyContent: 'flex-start' }
+            }
+        >
+            <div
+                className={`messageBox ${
+                    messages.msg_for === user ? 'sentMessageBox' : 'receivedMessageBox'
+                }`}
+            >
+                <p className="messageText">{messages.msg_content}</p>
             </div>
         </div>
     );
-}
+};
 
 export default MessageBox;
